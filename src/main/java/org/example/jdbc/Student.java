31de818d80d8fbc942066a3d969f82998ce8f77d
Subhransupanda2000 <imsubhransupanda2000@gmail.com>
2024-12-08 22:29:15 +0530

    getConnection implementation

---
+++ src/main/java/org/example/jdbc/Student.java
@@ -0,0 +1,19 @@
+package org.example.jdbc;
+
+import com.mysql.jdbc.Driver;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Student {
+    public static void main(String[] args) throws SQLException {
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/studentdb?user=root&password=system");
+        Statement st=con.createStatement();
+        st.execute("insert into student2 values(10,'john','john@gmail.com')");
+        System.out.println("Data inserted sucessfully");
+        st.close();
+        con.close();
+    }
+}
